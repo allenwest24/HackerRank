db46845e63a81d15c7bce571c4183385594b48ee
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-10-14 17:44:08 -0400

    Create solution.py

---
+++ Sales by Match/solution.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    pairs = 0
+    singles = []
+    for ii in range(n):
+        if ar[ii] in singles:
+            pairs += 1
+            singles.remove(ar[ii])
+        else:
+            singles.append(ar[ii])
+    return pairs
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
