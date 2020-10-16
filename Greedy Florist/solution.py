720c7fb11e88da79e4ce3b195807e8e136a4ae43
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-10-15 21:52:19 -0400

    Create solution.py

---
+++ Greedy Florist/solution.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    c.sort()
+    ii = len(c) - 1
+    mult = 0
+    total = 0
+    while ii >= 0:
+        for jj in range(k):
+            if ii < 0:
+                break
+            total += c[ii] * (mult + 1)
+            ii -= 1
+        mult += 1
+    return total
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
