cf16746b8942aac158c096b88ba42928bffd7b2c
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-10-18 12:03:27 -0400

    Create solution.py

---
+++ Mark and Toys/solution.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    total = 0
+    ii = 0
+    while k > 0:
+        if k >= prices[ii]:
+            total += 1
+            k -= prices[ii]
+        elif k < prices[ii]:
+            k = 0
+        ii += 1
+    return total
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
